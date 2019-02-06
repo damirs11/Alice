1159278e2dab6130d46d6b971116d9b077d21cdd
Alex Lopez <alexlpzrms@gmail.com>
2019-02-06 10:21:08 +0100

    Added MenuView

---
+++ app/src/main/java/com/alexlopezramos/arkanoid/View/MenuView.java
@@ -0,0 +1,70 @@
+package com.alexlopezramos.arkanoid.View;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.alexlopezramos.arkanoid.R;
+
+public class MenuView extends AppCompatActivity {
+
+    ImageView wallpaper, title;
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        wallpaper = (ImageView) findViewById(R.id.wallpaper);
+        title = (ImageView) findViewById(R.id.title);
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), GameView.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Ventana Emergente
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit");
+        builder.setMessage("Do you really want to exit?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                moveTaskToBack(true);
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked Cancel button
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
